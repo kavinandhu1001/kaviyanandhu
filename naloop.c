230862a1b1ac81a21c7ef25782aaa88038e1bbdf
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2017-09-02 23:15:10 -0400

    Create naloop.c

---
+++ naloop.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+void main()
+
+{
+  
+  int N, i, S= 0;
+ 
+   
+    printf("Enter a positive integer: ");
+
+    scanf("%d",&N);
+
+    for(i=1; i <= N; ++i)
+   
+ {
+    
+    S=S+i; 
+  }
+
+    
+printf("S= %d",S);
+
+    
+
+}
